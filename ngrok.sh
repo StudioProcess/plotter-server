2eb04e58d8e68dec2e656b8e6057e6efd8382731
Martin Grödl <martin@process.studio>
2022-11-17 18:41:16 +0000

    Add start script that runs ngrok and the server

--- ngrok.sh
+++ ngrok.sh
@@ -4,8 +4,10 @@ hostname=plotter.eu.ngrok.io
 if [[ $1 == 'http' ]]; then
     # connect to local HTTP server (NOT HTTPS)
     [[ $local_port == 0 ]] && local_port=80
-    ngrok http http://localhost:$local_port --hostname=$hostname
+    [[ $2 == 'noui' ]] && flag='--log=stdout'
+    ngrok http http://localhost:$local_port --hostname=$hostname $flag
 else
     [[ $local_port == 0 ]] && local_port=443
-    ngrok http https://localhost:$local_port --hostname=$hostname
+    [[ $1 == 'noui' ]] && flag='--log=stdout'
+    ngrok http https://localhost:$local_port --hostname=$hostname $flag
 fi
