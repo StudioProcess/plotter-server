d7725989a92e563ef3382a67862548f9067b40fe
Martin Grödl <martin@process.studio>
2024-01-10 11:33:36 +0100

    Updates scripts

--- frpc.sh
+++ frpc.sh
@@ -8,6 +8,6 @@ quit() {
 trap quit EXIT
 
 >&2 echo "Plotter URL (via frp): wss://plotter.process.tools"
-source ./frp.auth # read auth token from file
+source frp.auth # read auth token from file
 export FRP_AUTH_TOKEN
 frpc -c frpc.toml
