3c04cdba54dc14ba03f5096ffb6789f4264f33b1
Martin Grödl <martin@process.studio>
2024-04-11 16:07:01 +0200

    Fix frpc not found

--- frpc.sh
+++ frpc.sh
@@ -7,7 +7,19 @@ quit() {
 }
 trap quit EXIT
 
+# find frpc
+frpc=$(which "frpc")
+if [[ -z $frpc ]]; then
+    frpc=$(which "../frp-mac/latest/frpcx")
+    if [[ -z $frpc ]]; then
+        >&2 echo "frpc not found; try installing with 'brew install frpc'"
+        exit 1
+    fi
+fi
+
+>&2 echo "Using frpc: $frpc"
 >&2 echo "Plotter URL (via frp): wss://plotter.process.tools"
 source frp.auth # read auth token from file
 export FRP_AUTH_TOKEN
-frpc -c frpc.toml
+
+$frpc -c frpc.toml
